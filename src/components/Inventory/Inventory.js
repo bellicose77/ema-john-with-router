59f18ca80f9da7e3b758684428bd3a4a92b1ff71
bellicose77 <64400354+bellicose77@users.noreply.github.com>
2021-10-02 13:01:34 +0600

    ne

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>This is inventory pages:</h2>
+            <h2> Welcome to: This is inventory pages:</h2>
         </div>
     );
 };
